4154e6cdcf07a254f065e8b17a5c08870e06f8c7
low <907226763@qq.com>
2018-09-21 14:30:38 +0800

    mod: class ThrowableError

--- core/lib/ThrowableError.php
+++ core/lib/ThrowableError.php
@@ -5,27 +5,6 @@ class ThrowableError extends \ErrorException
 {
     public function __construct(\Throwable $e)
     {
-
-        if ($e instanceof \ParseError) {
-            $message  = 'Parse error: ' . $e->getMessage();
-            $severity = E_PARSE;
-        } elseif ($e instanceof \TypeError) {
-            $message  = 'Type error: ' . $e->getMessage();
-            $severity = E_RECOVERABLE_ERROR;
-        } else {
-            $message  = 'Fatal error: ' . $e->getMessage();
-            $severity = E_ERROR;
-        }
-
-        parent::__construct(
-            $message,
-            $e->getCode(),
-            $severity,
-            $e->getFile(),
-            $e->getLine()
-        );
-
-   
         
         $this->setTrace($e->getTrace());
     }
